db0b29558e157b5f0d547e2ee9c56bf23c58a087
ReneU <rene.u@uni-muenster.de>
2015-05-27 20:08:53 +0200

    Log GPS from shield

--- akeru-test-sketch_.ino
+++ akeru-test-sketch_.ino
@@ -1,61 +1,56 @@
+/**
+Arduino GPS Client to retrieve the Location data from the GPS shield.
+The information are processed through the TinyGPS library and logged to the console
+**/
+
 #include <SoftwareSerial.h>
-#include <Akeru.h>
+#include <TinyGPS.h>
 
-/* 
- * Akeru Libary at: http://snootlab.com/lang-en/shields-snootlab/547-akeru-beta-32-fr.html
- * or at: https://github.com/hackerspacesg/SIGFOX
- */
- 
-// Initialize the modem:
-void setup() {
+TinyGPS gps;
+SoftwareSerial ss(0,1);
 
-  // initialize serial communication at 9600 bits per second:
+void setup()
+{
+  ss.begin(9600);
   Serial.begin(9600);
-  Serial.println("Starting...");
-
-  // Wait 1 second for the modem to warm up
-  delay(1000);
-
-  // Init modem
-  Akeru.begin();
-  
+  Serial.print("Finished setup");
 }
 
-typedef struct {
-  int lat;
-  int lon;
-  int counter;
-} Payload;
-
-
-void loop() { 
-  
- Payload p;
+void loop()
+{ 
+  bool newData = false;
+  unsigned long chars;
+  unsigned short sentences, failed;
  
- p.counter = 0;
- p.lat = 51;
- p.lon = 7;
-  
-   if(!Akeru.isReady()) {
-      Serial.println("Modem not ready");
-      delay(1000);
-    } else {
-      Serial.println("Modem ready");
-      delay(1000);
-      
-      if (p.counter==0) {
-        // Send data:
-        Serial.println(sizeof(p.lat));
-        Serial.println(sizeof(p.lon));
-        Serial.println(sizeof(p));
-        Akeru.send(&p, sizeof(p));
-        Serial.println("Message sent");
-        delay(1000);
-        
-        p.counter++;
-      
-    } else {
-      Serial.println("Counter>0");
+  // parse GPS data for one second and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;)
+  {
+    while (ss.available())
+    {
+      char c = ss.read();
+      //Serial.print(c); //uncomment to see the full NMEA datasets
+      if (gps.encode(c)) // Did a new valid sentence come in?
+        Serial.println("New Data!");
+        newData = true;
     }
+  }
+    
+    if (newData)
+  {
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.println("");      
+    Serial.print("LAT = ");  
+    Serial.println(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print("LON = ");
+    Serial.println(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print("HDOP = ");
+    Serial.println(gps.hdop()/100.0);
+    Serial.print("SAT = ");
+    Serial.println(gps.satellites());
+    Serial.print("Speed = ");
+    Serial.println(gps.f_speed_kmph());
+    newData = false;
   }
 }
